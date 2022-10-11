5f4d2b30c89e99bf267b761519f1ebab1c9b5506
unknown <mohmmdabouzaid@gmail.comm>
2022-10-11 15:35:55 +0200

    add middlewares

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+//port number
 const port = 3000;
+// create instance serer
+const app = express();
+
+//middlewares
+// parse incoming request midleware
+app.use(express.json());
+//http security middleware
+app.use(helmet());
+//HTTP request logger middleware
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("hello world");
